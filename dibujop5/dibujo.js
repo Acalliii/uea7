ce074d39391d56e4e91da7b74218aa6c5ed24fdd
Acalliii <nazarioacalli@gmail.com>
2025-11-29 15:10:36 -0600

    dibujo

---
+++ dibujop5/dibujo.js
@@ -0,0 +1,32 @@
+function setup() {
+  createCanvas(1350,630)
+  background(255,255,255);
+}
+function draw() {
+  ellipse(356,50,80);
+   ellipse(920,175,60);
+ ellipse(150,300,45);
+ ellipse(400,220,100);
+ ellipse(760,485,80);
+  ellipse(660,280,130);
+ ellipse(800,25,100);
+ ellipse(360,400,86);
+ ellipse(80,100,200);
+  ellipse(150,520,120);
+  ellipse(1000,400,200);
+ ellipse(1200,120,130);
+ ellipse(550,600,150);
+ ellipse(1200,600,80);
+ellipse(1350,400,160);
+fill(0)
+line(1350,100,700,100);
+line(850,400,300,360);
+line(300,600,160,520);
+line(900,640,73,60);
+line(1200,800,380,42);
+line(4000,730,485,180);
+}
+
+
+
+
